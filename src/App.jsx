3fe5deeda909858c4bec20486794772b03e94d5e
Abdullah Rayean <rayeanabdullah@gmail.com>
2025-04-07 01:12:18 +0600

    All files are added

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Blogs from './Components/Blogs/blogs'
+import Navbar from './Components/Navbar/Navbar'
+
+
+function App() {
+
+  return (
+    <>
+      <Navbar></Navbar>
+      <Blogs></Blogs>
+    </>
+  )
+}
+
+export default App
